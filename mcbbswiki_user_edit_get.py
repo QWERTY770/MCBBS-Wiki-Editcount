9d39d23854d5677b070a9060742c6a6a0821d2da
Justin Zhang <2691475984@qq.com>
2022-12-25 09:47:03 +0800

    update 2022-12-25

--- mcbbswiki_user_edit_get.py
+++ mcbbswiki_user_edit_get.py
@@ -6,13 +6,16 @@ from retrying import retry
 import json
 import os
 from time import localtime
+import threading
 
 
 __author__ = "QWERTY_52_38"
-__version__ = "0.2"
-rev_api = "https://mcbbs-wiki.cn/api.php?action=query&format=json&prop=revisions&revids="
+__version__ = "0.5"
+rev_api = "https://mcbbs.wiki/api.php?action=query&format=json&prop=revisions&revids="
 # revisions api
-folder = r"D:\python\mcbbswiki\MCBBS-Wiki-Editcount"
+folder = r"D:\python\mcbbswiki\MCBBS-Wiki-Editcount"  # this should be changed to your own directory
+headers = {"user-agent": 'QWERTY770/1.0 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52'}
+# avoid returning 418
 
 namespace_score = {0: 3, 1: 0.125, 4: 1, 5: 0.125,
                    10: 2.5, 11: 0.125, 12: 2, 13: 0.125, 14: 1, 15: 0.125}
@@ -23,14 +26,16 @@ namespace_loca = {0:0, 1: 1, 4: 2, 5: 1, 10: 3,
 
 @retry(stop_max_attempt_number=10)
 def get_page(url: str):
-    return get(url, timeout=5).text
+    return get(url, timeout=5, headers=headers).text
 
 
 def get_revs(start, end):
+    print(f"{start} to {end} started!\n", end="")
     for i in range(start, end + 1):
+        rev = get_page(rev_api + str(i))
         with open(os.path.join(os.path.join(folder, "rev"), f"rev_{i}.txt"), "w") as f:
-            f.write(get_page(rev_api + str(i)))
-        print(i)
+            f.write(rev)
+    print(f"{start} to {end} finished!\n", end="")
 
 
 def get_edit_score_dic(start: int, end: int) -> dict:
@@ -47,10 +52,11 @@ def get_edit_score_dic(start: int, end: int) -> dict:
             except:
                 continue
             if user not in user_dic:
-                user_dic[user] = [0,0,0,0,0,0,0] # main talk mcbbswiki template help category score
+                user_dic[user] = [0,0,0,0,0,0,0,0] # main talk mcbbswiki template help category score total
             if namespace in namespace_score:
-                    user_dic[user][namespace_loca[namespace]] += 1
-                    user_dic[user][6] += namespace_score[namespace]
+                user_dic[user][namespace_loca[namespace]] += 1
+                user_dic[user][-2] += namespace_score[namespace]
+            user_dic[user][-1] += 1
     return user_dic
 
 
@@ -70,7 +76,7 @@ def make_workbook(dic: dict, filename=f"mcbbswiki-useredit-{localtime().tm_year}
 
     for m, i in enumerate(dic.keys()):
         ws.cell(row=m+2, column=1).value = i
-        ws.cell(row=m+2, column=2).value = sum(dic[i][0:6])
+        ws.cell(row=m+2, column=2).value = dic[i][-1]
         for n, j in enumerate(dic[i]):
             ws.cell(row=m+2, column=n+3).value = j
     
@@ -79,6 +85,6 @@ def make_workbook(dic: dict, filename=f"mcbbswiki-useredit-{localtime().tm_year}
 
 
 if __name__ == "__main__":
-    # get_revs(34511, 34925)
-    make_workbook(get_edit_score_dic(1, 34925))
+    # get_revs(41025, 41029)
+    make_workbook(get_edit_score_dic(1, 41029))
     print("Finished!")
